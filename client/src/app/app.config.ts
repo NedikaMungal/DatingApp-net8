5a183c9c6a9e5147eee72065582d713927a789e8
Nedika <nedika.mungal@gmail.com>
2024-10-02 13:12:05 +0200

    End of section 5

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,5 +1,7 @@
-import{ ApplicationConfig } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
@@ -7,5 +9,6 @@ export const appConfig: ApplicationConfig = {
   providers: 
   [provideRouter(routes),
   provideHttpClient(),
+  provideAnimations()
 ]
 };
